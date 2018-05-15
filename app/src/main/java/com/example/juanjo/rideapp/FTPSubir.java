b50bc2e3b0c5042987048f9bd35f21a160ca7556
Unknown <richardal.94@gmail.com>
2018-05-15 18:14:26 +0200

    Merge branch 'DesarrolloFTP'
    FTP integrado

---
+++ app/src/main/java/com/example/juanjo/rideapp/FTPSubir.java
@@ -0,0 +1,138 @@
+package com.example.juanjo.rideapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.SocketException;
+
+public class FTPSubir extends AsyncTask<File , Integer, Long> {
+    private static final int SELECT_FILE = 1;
+    //Credenciales
+    private String ip =  "rideapp.somee.com";
+    public static  String USUARIO = "jesus93";				//Almacena el usuario
+    public static String PASS = "rideapp@M";			//Almacena la contraseña
+
+    FTPClient ftpClient;            //Crea la conexión con el servidor
+    BufferedInputStream bufferIn;   //Crea una buffer de lectura
+    BufferedOutputStream bufferOut; //Crea una buffer de escritura
+    File rutaCompleta;                //Almacena la ruta completa del archivo
+    Context context;                //Almacena el contexto de la aplicacion
+
+    /**
+     * Crea una instancia de FTP sin credenciales
+     */
+    public FTPSubir(Context context) {
+
+        //Inicialización de campos
+        ftpClient = null;
+        bufferIn = null;
+        bufferOut = null;
+        rutaCompleta = null;
+
+        this.context = context;
+    }
+
+    /**
+     * Realiza el login en el servidor
+     * @return	Verdad en caso de haber realizado login correctamente
+     * @throws SocketException
+     * @throws IOException
+     */
+    public boolean login () throws IOException {
+        //Establece conexión con el servidor
+        System.out.println("Conectando . . .");
+        try{
+            ftpClient = new FTPClient();
+            ftpClient.connect(ip);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            //Informa al usuario
+            System.out.println("Imposible conectar . . .");
+            return false;	//En caso de que no sea posible la conexion
+        }
+        //Hace login en el servidor
+        ftpClient.enterLocalPassiveMode();
+
+        if (ftpClient.login(USUARIO, PASS)){
+            //Informa al usuario
+            System.out.println("Login correcto . . .");
+            return true;	//En caso de login correcto
+        }
+        else{
+            //Informa al usuario
+            System.out.println("Login incorrecto . . .");
+            return false;	//En caso de login incorrecto
+        }
+    }
+
+    /**
+     *  Sube un archivo al servidor FTP si previamente se ha hecho login correctamente
+     * @return	Verdad en caso de que se haya subido con éxito
+     * @throws IOException
+     */
+    public boolean SubirArchivo () throws IOException{
+
+        ftpClient.enterLocalPassiveMode();
+        ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+        //Cambia la carpeta Ftp
+        if (ftpClient.changeWorkingDirectory("./www.rideapp.somee.com/Imagenes/")){
+
+            //Informa al usuario
+            System.out.println("Carpeta ftp cambiada . . .");
+
+            //Crea un buffer hacia el servidor de subida
+            bufferIn = new BufferedInputStream(new FileInputStream(rutaCompleta));
+
+            if (ftpClient.storeFile(rutaCompleta.getName(), bufferIn)){
+                //Informa al usuario
+                System.out.println("Archivo subido . . .");
+                bufferIn.close();		//Cierra el bufer
+                return true;		//Se ha subido con éxito
+            }
+            else{
+                //Informa al usuario
+                System.out.println("Imposible subir archivo . . .");
+                bufferIn.close();		//Cierra el bufer
+                return false;		//No se ha subido
+            }
+        }
+        else{
+            //Informa al usuario
+            System.out.println("Carpeta ftp imposible cambiar . . .");
+            return false;		//Imposible cambiar de directo en servidor ftp
+        }
+    }
+
+    @Override
+    protected Long doInBackground(File... files) {
+        try {
+            for (int i = 0; i < files.length; i++){
+                rutaCompleta = files[0];
+            }
+            login();
+            SubirArchivo();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Imposible conectar . . .");
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Long result) {
+        //Termina proceso
+        Log.i("TAG" , "Termina proceso de lectura de archivos.");
+    }
+
+}
--- app/src/main/java/com/example/juanjo/rideapp/SplashScreen.java
+++ app/src/main/java/com/example/juanjo/rideapp/SplashScreen.java
@@ -1,30 +1,13 @@
 package com.example.juanjo.rideapp;
 
-import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
-    // Duración en milisegundos que se mostrará el splash
-    private final int DURACION_SPLASH = 3000; // 3 segundos
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Tenemos una plantilla llamada splash.xml donde mostraremos la información que queramos (logotipo, etc.)
         setContentView(R.layout.activity_splash_screen);
-
-        new Handler().postDelayed(new Runnable(){
-            public void run(){
-                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
-                Intent intent = new Intent(SplashScreen.this, Login.class);
-                startActivity(intent);
-                finish();
-            };
-        }, DURACION_SPLASH);
     }
-
 }
