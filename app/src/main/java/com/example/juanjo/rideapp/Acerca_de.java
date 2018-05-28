e793fe7fd4318bfc90214f810b00a76985c5516d
JuanjoAvila <juanjo.avila.chavero@gmail.com>
2018-05-28 16:54:49 +0200

    Acerca de operativo

--- app/src/main/java/com/example/juanjo/rideapp/Acerca_de.java
+++ app/src/main/java/com/example/juanjo/rideapp/Acerca_de.java
@@ -18,9 +18,7 @@ public class Acerca_de extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
         View descorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE;
         descorView.setSystemUiVisibility(uiOptions);
